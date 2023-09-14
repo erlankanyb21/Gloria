574daccd61c35d79954349c74d2b0830253f3537
Erlan <kanybekov668@gmail.com>
2023-09-14 16:31:50 +0600

    ss

--- common/main/compose/src/androidMain/kotlin/more/faq/FaqScreen.kt
+++ common/main/compose/src/androidMain/kotlin/more/faq/FaqScreen.kt
@@ -1,34 +1,26 @@
 package more.faq
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import components.BasicExpandableCard
 import components.ToolBar
+import org.tbm.gloria.main.compose.R.string.questions_answers
 import org.tbm.gloria.core_compose.R
 import ru.alexgladkov.odyssey.compose.local.LocalRootController
 
@@ -39,20 +31,22 @@ fun FAQScreen() {
 
     Scaffold(
         topBar = {
-            ToolBar(title = "Вопрос-ответы", backIcon = {
-                IconButton(
-                    modifier = Modifier.size(50.dp),
-                    colors = IconButtonDefaults.iconButtonColors(
-                        contentColor = Color.White
-                    ),
-                    onClick = { rootController.popBackStack() },
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_back_arrow),
-                        contentDescription = null,
-                    )
-                }
-            })
+            ToolBar(
+                title = stringResource(id = questions_answers),
+                backIcon = {
+                    IconButton(
+                        modifier = Modifier.size(50.dp),
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = Color.White
+                        ),
+                        onClick = { rootController.popBackStack() },
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_back_arrow),
+                            contentDescription = null,
+                        )
+                    }
+                })
         }
     ) {
         LazyColumn(
